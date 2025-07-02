474ce3caf02d8d786218a4b6d478f407e81d17bd
kdk0508 <donggyugim321@gmail.com>
2025-07-02 14:01:32 +0900

    add all

---
+++ Assets/Scenes/Spawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float intervalMin = 0.5f;
+    public float intervalMax = 3f;
+
+    private Transform target;
+    private float interval;
+    private float timeAfterSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interval = Random.Range(intervalMin, intervalMax);
+        timeAfterSpawn = 0f;
+
+        target = FindObjectOfType<Player>().transform;
+
+        //target = GameObject.Find("Player").transform;
+
+        //target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if(timeAfterSpawn >= interval)
+        {
+            GameObject bullet
+                = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+
+            interval = Random.Range(intervalMin, intervalMax);
+            timeAfterSpawn = 0f;
+        }
+    }
+}
